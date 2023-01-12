541aa1b0815f0b86e4b30936ff486f59ddc25a29
camille <cerulean.dsgn@gmail.com>
2023-01-12 20:55:41 -0300

    post participants status201, status409 e status500

--- back/src/app.js
+++ back/src/app.js
@@ -1,4 +1,4 @@
-import express, { application } from "express"
+import express from "express"
 import cors from "cors"
 import dotenv from 'dotenv'
 import { MongoClient } from 'mongodb'
@@ -6,11 +6,16 @@ import dayjs from 'dayjs'
 
 
 dotenv.config();
+const server = express()
+server.use(express.json())
+server.use(cors())
 
 const mongoClient = new MongoClient(process.env.DATABASE_URL)
-let db;
+let db; // armazena variavel para usar conexoes do mongoClient
 const PORT = 5000;  
-let hour = dayjs().format("HH:mm:ss")
+
+
+// let hour = dayjs().format("HH:mm:ss")
 
 //formato participante
 //{name: 'João', lastStatus: 12313123} // O conteúdo do lastStatus será explicado nos próximos requisitos
@@ -20,21 +25,28 @@ let hour = dayjs().format("HH:mm:ss")
 
 
 try {
-    await mongoClient.connect()
+    await mongoClient.connect() //evita callbackhell -> esta é uma top lvl await
     db = mongoClient.db() 
     console.log("Conectado")
 } catch (err) {
     console.log(err.message)
 }
 
-const server = express()
-server.use(express.json())
-server.use(cors())
 
 
-server.post("/participants", (req,res) => {
+server.post("/participants", async (req,res) => {
     const { name } = req.body //recebe parametro name a ser cadastrado
+    console.log(name);
+    try {
+        const userExist = await db.collection("participants").findOne({name})
+        if (userExist) return res.status(409).send("Este usuário já existe") //impedir cadastro de usuario ja existente
 
+        await db.collection("participants").insertOne({name: name, lastStatus: Date.now()}) // salvar na coleção participants
+        res.status(201).send("ok") //retornar status 201 pode retirar esse send ok 
+    } catch (err) {
+        console.log(err)
+        res.status(500).send("Deu algo errado no servidor")
+    }
 
 })
 
