16f9c77a61420ba897afe7623670ffdb34cf43d2
camille <cerulean.dsgn@gmail.com>
2023-01-13 23:34:32 -0300

    requisito+validação post participants e message ok

--- back/src/app.js
+++ back/src/app.js
@@ -3,6 +3,7 @@ import cors from "cors"
 import dotenv from 'dotenv'
 import { MongoClient } from 'mongodb'
 import dayjs from 'dayjs'
+import joi from 'joi'
 
 
 dotenv.config();
@@ -14,6 +15,7 @@ const mongoClient = new MongoClient(process.env.DATABASE_URL)
 let db; // armazena variavel para usar conexoes do mongoClient
 const PORT = 5000;  
 let hour = dayjs().format("HH:mm:ss")
+
 
 //formato participante
 //{name: 'João', lastStatus: 12313123} // O conteúdo do lastStatus será explicado nos próximos requisitos
@@ -29,12 +31,28 @@ try {
 } catch (err) {
     console.log(err.message)
 }
+//valida user sem string
+const userSchema = joi.object({
+    name: joi.string().required()
+})
 
+//valida mensagem to text type
+const messageSchema = joi.object({
+    to: joi.string().required(),
+    text: joi.string().required(),
+    type: joi.string().valid('message', 'private_message').required()
+})
 
-
+//// inicio http verbs
 server.post("/participants", async (req,res) => {
     const { name } = req.body //recebe parametro name a ser cadastrado
     console.log(name);
+
+    const userValidate = userSchema.validate({name}) //validaçao 422
+    if (userValidate.error){
+        return res.sendStatus(422)
+    }
+
     try {
         const userExist = await db.collection("participants").findOne({name})
         if (userExist) return res.status(409).send("Este usuário já existe") //impedir cadastro de usuario ja existente
@@ -53,8 +71,8 @@ server.post("/participants", async (req,res) => {
         res.status(500).send("Deu algo errado no servidor")
     }
 
-    //necessario validar status 422 (name nao vazio) - joi
-    //salvar a mensagem no formato acima usar dayjs
+
+ 
 
 
 })
@@ -65,13 +83,22 @@ server.get("/participants", async (req,res) => {
   // requisito get participants ok
 })
 
-server.post("/messages", (req,res) => {
+server.post("/messages", async (req,res) => {
     const { to, text, type } = req.body // recebe os parametros no body da request
-    const from = req.headers.user // recebe os parametros do front (headers)
+    const from  = req.headers.user // recebe os parametros do front (headers)
     console.log(to);
     console.log(text);
     console.log(type);
     console.log(from);
+    const messageValidate = messageSchema.validate({to, text, type})
+
+    if(messageValidate.error){
+        return res.sendStatus(422)
+    }
+
+    if (!await db.collection("participants").findOne({ name: from })){
+        return res.status(422).send("Participante inexistente")
+    }
 
     try{
         db.collection("messages").insertOne({ from, to, text, type, time:hour}) // salvar time no formato requerido usando dayjs
@@ -80,21 +107,45 @@ server.post("/messages", (req,res) => {
     } catch (err) {
         res.status(500).send("erro na msg")
     }
-  //falta validar via joi status 422
+//requisito post message ok
 })
 
-server.get("/messages", (req,res) => {
+server.get("/messages", async (req,res) => {
+    try{
+        const query = req.query;
+        // const { user } = req.headers;
+        const participant = await db.collection("participants").find({name: req.headers.user})
+        // const typeMsg = {
+        //     $or: [ {type: "status"}, {type: "message"}, {type: "private_message", from: participant.name}, { type: "private_message", from: participant.name}]
+        // }
+        
+        // const limitMsg = parseInt(req.query.limit) //query string limit
+        const message = await db.collection("message").find().toArray(); //retorna a msg
+        // if(limitMsg) {
+        //     return res.send(message.slice(-limitMsg).reverse());
+        // }
 
+        return res.send(message);
+    } catch (err) {
+        console.log(err)
+    res.status(500).send("erro no get das msgs")
+    }
 })
 
-server.post("/status", (req,res) => {
-
+server.post("/status", async (req,res) => {
+    try{
+        const user = req.headers.user; // recebe requisição user
+        const participantIsOnline = await db.collection("participants").findOne({ name:user })
+        if (!participantIsOnline) return res.status(404).send("Esse usuário não existe!")
+    } catch(err) {
+        return res.status(500).send("erro no post status")
+    }
 })
 
 
-// function removeUser(){
+function removeUser(){
 
-// } // requisito remover usuário inativo lastStatus mais que 10 segundos atrás;
+} // requisito remover usuário inativo lastStatus mais que 10 segundos atrás;
 
 
 server.listen(PORT, () => {
